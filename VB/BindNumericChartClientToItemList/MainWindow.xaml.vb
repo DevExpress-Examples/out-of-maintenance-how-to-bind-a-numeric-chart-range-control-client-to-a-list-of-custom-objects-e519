9f2f4868b90b0a9f4fdc42ce2fb6f858bb00a767
DevExpressExampleBot <fakeEmail>
2021-09-01 18:00:18 +0300

    Source auto update [skip ci]

--- VB/BindNumericChartClientToItemList/MainWindow.xaml.vb
+++ VB/BindNumericChartClientToItemList/MainWindow.xaml.vb
@@ -1,29 +1,12 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Windows
 
 Namespace BindNumericChartClientToItemList
 
 	Public Class NumericItem
-		Private privateArgument As Object
 		Public Property Argument() As Object
-			Get
-				Return privateArgument
-			End Get
-			Set(ByVal value As Object)
-				privateArgument = value
-			End Set
-		End Property
-		Private privateValue As Object
 		Public Property Value() As Object
-			Get
-				Return privateValue
-			End Get
-			Set(ByVal value As Object)
-				privateValue = value
-			End Set
-		End Property
 	End Class
 
 	Partial Public Class MainWindow
@@ -35,7 +18,10 @@ Namespace BindNumericChartClientToItemList
 
 		Private Function GenerateNumericData() As List(Of NumericItem)
 			For i As Integer = 0 To dataCount - 1
-				data.Add(New NumericItem() With {.Argument = i, .Value = rand.Next(0, 30) + i})
+				data.Add(New NumericItem() With {
+					.Argument = i,
+					.Value = rand.Next(0, 30) + i
+				})
 			Next i
 			Return data
 		End Function
