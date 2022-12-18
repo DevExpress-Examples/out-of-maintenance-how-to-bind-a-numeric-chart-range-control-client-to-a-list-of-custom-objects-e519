82761df52156f99a97c822aad578857148954e97
DevExpressExampleBot <fakeEmail>
2022-12-18 15:44:56 +0400

    Source auto update [skip ci]

--- VB/BindNumericChartClientToItemList/MainWindow.xaml.vb
+++ VB/BindNumericChartClientToItemList/MainWindow.xaml.vb
@@ -1,34 +1,36 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Windows
 
 Namespace BindNumericChartClientToItemList
 
-	Public Class NumericItem
-		Public Property Argument() As Object
-		Public Property Value() As Object
-	End Class
+    Public Class NumericItem
 
-	Partial Public Class MainWindow
-		Inherits Window
+        Public Property Argument As Object
 
-		Private Const dataCount As Integer = 10
-		Private data As New List(Of NumericItem)()
-		Private rand As New Random()
+        Public Property Value As Object
+    End Class
 
-		Private Function GenerateNumericData() As List(Of NumericItem)
-			For i As Integer = 0 To dataCount - 1
-				data.Add(New NumericItem() With {
-					.Argument = i,
-					.Value = rand.Next(0, 30) + i
-				})
-			Next i
-			Return data
-		End Function
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			Me.DataContext = GenerateNumericData()
-		End Sub
-	End Class
+        Const dataCount As Integer = 10
+
+        Private data As List(Of NumericItem) = New List(Of NumericItem)()
+
+        Private rand As Random = New Random()
+
+        Private Function GenerateNumericData() As List(Of NumericItem)
+            For i As Integer = 0 To dataCount - 1
+                data.Add(New NumericItem() With {.Argument = i, .Value = rand.Next(0, 30) + i})
+            Next
+
+            Return data
+        End Function
+
+        Public Sub New()
+            Me.InitializeComponent()
+            DataContext = GenerateNumericData()
+        End Sub
+    End Class
 End Namespace
